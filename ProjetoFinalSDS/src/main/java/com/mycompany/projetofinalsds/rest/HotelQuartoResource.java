0994e1ee17d4676b5ff85e1b976b8c8cc5328d64
raul.neto <rauljose280@gmail.com>
2019-06-12 10:18:38 -0300

    Método PUT quarto hotel

--- ProjetoFinalSDS/src/main/java/com/mycompany/projetofinalsds/rest/HotelQuartoResource.java
+++ ProjetoFinalSDS/src/main/java/com/mycompany/projetofinalsds/rest/HotelQuartoResource.java
@@ -2,12 +2,10 @@ package com.mycompany.projetofinalsds.rest;
 
 import com.mycompany.projetofinalsds.dao.DAO;
 import com.mycompany.projetofinalsds.model.HotelQuarto;
-import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
-import javax.persistence.Entity;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -21,10 +19,9 @@ public class HotelQuartoResource {
     private DAO<HotelQuarto> dao;
 
     @PUT
-    @Path("{hotelQuarto}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public void save(@PathParam("{hotelQuarto}")Entity entity) {
-        dao.save((HotelQuarto) entity);
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void update(HotelQuarto quarto) {
+        dao.save(quarto);
     }
 
 }
